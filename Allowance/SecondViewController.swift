f6ddc1179dc711106a5af74bfb5a65accb3cedb4
Nick Lanasa <nick@nytekproduction.com>
2015-09-11 21:33:04 -0400

    Initial Commit

---
+++ Allowance/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Allowance
+//
+//  Created by Nickolas Lanasa on 9/11/15.
+//  Copyright (c) 2015 Nytek Productions. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
